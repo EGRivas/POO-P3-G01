5cec703872f52187caddc7cfd177be9896dc5d66
JeanCaAguilarT <jcaguila@espol.edu.ec>
2023-08-20 19:29:07 -0500

    "Ventana de edición de termino"

--- JuegoImPOOsible/src/main/java/com/juegoimpoosible/EdiTermController.java
+++ JuegoImPOOsible/src/main/java/com/juegoimpoosible/EdiTermController.java
@@ -5,8 +5,10 @@ import com.juegoimpoosible.modelo.Termino;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-
+import com.juegoimpoosible.funcionalidad.Archivar;
+import com.juegoimpoosible.modelo.Termino;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -14,18 +16,34 @@ import java.util.ResourceBundle;
 
 public class EdiTermController implements Initializable {
 
-    @FXML
-    private ChoiceBox<Termino> termChoiceBox; // Cambia el tipo a Termino
 
+    public ListView termListView;
     @FXML
     private TextField periodoTextField;
     @FXML
     private TextField anioTextField;
-
-    @Override
+    @FXML
     public void initialize(URL url, ResourceBundle rb) {
         ArrayList<Termino> terms = Archivar.readTerms();
-        termChoiceBox.getItems().addAll(terms); // Agrega los términos al ChoiceBox
+        termListView.getItems().addAll(terms);
+
+        termListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                periodoTextField.setText(((Termino)newValue).getPeriodo());
+                anioTextField.setText(((Termino)newValue).getAnio());
+            }
+        });
     }
+
+    @FXML
+    private void handleGuardarButton() {
+        Termino selectedTerm = (Termino) termListView.getSelectionModel().getSelectedItem();
+        if (selectedTerm != null) {
+            selectedTerm.setPeriodo(periodoTextField.getText());
+            selectedTerm.setAnio(anioTextField.getText());
+            termListView.refresh(); // Actualizar la vista del ListView
+        }
+    }
+
 
 }
